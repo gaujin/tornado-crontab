76983cc01bf534a130e5232dd7fc28a22dfe5fa7
Takehito Yamada <takehito@gaujin.jp>
2015-11-15 13:33:17 +0900

    Bump master version number to 0.2.dev1

--- tornado_crontab/__init__.py
+++ tornado_crontab/__init__.py
@@ -1,4 +1,4 @@
-from ._crontab import CronTabCallback
+from ._crontab import CronTabCallback, crontab
 
-version = "0.1"
-version_info = (0, 1, 0, 0)
+version = "0.2.dev1"
+version_info = (0, 1, 0, -100)
