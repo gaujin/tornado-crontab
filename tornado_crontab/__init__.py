09858def85f455f697a6e4e28f6cc6813f9d96c9
Takehito Yamada <takehito@gaujin.jp>
2017-10-07 13:53:49 +0900

    Version_info referring to version has been added to __ init__.py.

--- tornado_crontab/__init__.py
+++ tornado_crontab/__init__.py
@@ -1,3 +1,6 @@
 from ._crontab import CronTabCallback, crontab
 
-version = "0.4.dev1"
+__all__ = ["CronTabCallback", "crontab", "version", "version_info"]
+
+version = "0.3.3.dev1"
+version_info = tuple([int(v) for v in version.split(".")[:3]])
