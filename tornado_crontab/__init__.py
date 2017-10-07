db8bbbd2760aa216d131988d893e397ccfed442b
Takehito Yamada <takehito@gaujin.jp>
2017-10-07 21:09:10 +0900

    Set version number to 0.3.3

--- tornado_crontab/__init__.py
+++ tornado_crontab/__init__.py
@@ -2,5 +2,5 @@ from ._crontab import CronTabCallback, crontab
 
 __all__ = ["CronTabCallback", "crontab", "version", "version_info"]
 
-version = "0.3.3.dev1"
+version = "0.3.3"
 version_info = tuple([int(v) for v in version.split(".")[:3]])
