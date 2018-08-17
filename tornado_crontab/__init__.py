c8f2916bea94a0a92e5659a148c335343540a32e
Takehito Yamada <takehito@gaujin.jp>
2018-08-17 15:00:48 +0900

    Set version number to 0.4.0. #17

--- tornado_crontab/__init__.py
+++ tornado_crontab/__init__.py
@@ -2,5 +2,5 @@ from ._crontab import CronTabCallback, crontab
 
 __all__ = ["CronTabCallback", "crontab", "version", "version_info"]
 
-version = "0.4.0.dev1"
+version = "0.4.0"
 version_info = tuple([int(v) for v in version.split(".")[:3]])
